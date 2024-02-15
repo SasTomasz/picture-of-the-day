828b35252b6920433abea4c6044fd24eeb40e6ff
Tomasz Sas <tomaszsas.dev@gmail.com>
2024-02-15 09:17:40 +0100

    Show target picture, description and title

--- api_helper.py
+++ api_helper.py
@@ -1,9 +1,22 @@
 import requests
 import streamlit as st
 
+# Get data from apod
 api_key = st.secrets['API_KEY']
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
-response = requests.get(url)
+response = requests.get(url).json()
+
+# Get image
+image_url = response['hdurl']
+image = requests.get(image_url).content
 
 with open("./assets/images/apod.jpg", "wb") as file:
-    file.write(response.content)
+    file.write(image)
+
+with open("./assets/data/apod.txt", "w") as file:
+    file.write(response['explanation'])
+
+with open("./assets/data/apod_title.txt", "w") as file:
+    file.write(response['title'])
+
+print(response)
--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import streamlit as st
 
-lorem_ipsum = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis "
-               "accumsan urna quis magna condimentum, sed faucibus velit auctor. "
-               "Vestibulum sodales, turpis ac lobortis imperdiet, velit enim "
-               "consequat libero, sed tincidunt eros elit eget elit. "
-               "Pellentesque gravida mauris posuere pretium efficitur. "
-               "Donec sit amet aliquet nunc. Nunc orci ex, pellentesque "
-               "malesuada felis ut, venenatis facilisis velit. Aenean purus "
-               "massa, pulvinar nec mi ut, laoreet bibendum nulla. Nulla "
-               "sodales maximus elit, eu auctor mi fermentum sit amet. "
-               "Mauris ac turpis ligula. Nam dictum mollis urna, vitae "
-               "finibus diam. Mauris vitae quam vel dui aliquet feugiat in et "
-               "eros. Donec non lobortis erat.")
+with open("./assets/data/apod.txt") as file:
+    image_description = file.read()
 
-st.header('Beautiful picture', divider='rainbow')
-st.image("./assets/images/CarinaNebulaWebb.jpg")
-st.write(lorem_ipsum)
+with open("./assets/data/apod_title.txt") as file:
+    title = file.read()
+
+st.header(title, divider='rainbow')
+st.image("./assets/images/apod.jpg")
+st.write(image_description)
