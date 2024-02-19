861576fb34c8b832a1d0c291614fef81b800ea54
tomasz_sas <tomaszsas.dev@gmail.com>
2024-02-19 08:58:39 +0100

    Add logger

---
+++ app_logger.py
@@ -0,0 +1,21 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import sys
+
+
+def get_logger():
+    log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
+    log_file = "./app_logs/logs.log"
+
+    my_handler = RotatingFileHandler(log_file, mode='a', maxBytes=5*1024*1024, backupCount=2, encoding='utf-8', delay=False)
+    my_handler.setFormatter(log_formatter)
+    my_handler.setLevel(logging.DEBUG)
+
+    app_logger = logging.getLogger('root')
+    app_logger.setLevel(logging.DEBUG)
+
+    app_logger.addHandler(my_handler)
+
+    # Show logs in standard output
+    app_logger.addHandler(logging.StreamHandler(sys.stdout))
+    return app_logger
