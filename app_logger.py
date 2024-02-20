b1e0d288fb78843729d90f586f7339b411251980
tomasz_sas <tomaszsas.dev@gmail.com>
2024-02-20 08:03:48 +0100

    Check the log directory and if it does not exist, create it

--- app_logger.py
+++ app_logger.py
@@ -1,10 +1,14 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import sys
+import os
 
 
 def get_logger():
     log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
+    logs_dir = "./app_logs"
+    if not os.path.isdir(logs_dir):
+        os.makedirs(logs_dir)
     log_file = "./app_logs/logs.log"
 
     my_handler = RotatingFileHandler(log_file, mode='a', maxBytes=5*1024*1024, backupCount=2, encoding='utf-8', delay=False)
